8e25ce33fde6f2ec89a3b4b0820b5b3434b2a9fa
Amay <amayrana86@gmail.com>
2026-06-22 12:58:41 +0530

    changes some things , just saving

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const fs = require('fs/promises');
 
+//1.this block converts url into html
 async function getdata(url){
-    const response = await axios.get(url,{
+     const response = await axios.get(url,{
         headers: {
             'User-Agent':'mycrawlerbot/1.0'
         },
@@ -12,7 +13,8 @@ async function getdata(url){
     return response.data;
 }
 
-async function parsedata(html){
+//2.this block converts the given html and take out its links  and return it
+async function crawldata(html){
 const $ = cheerio.load(html);
 const links =[];
 
@@ -31,14 +33,58 @@ $('a').each((index,element) =>{
 return links;
 
 }
-
-async function main(){
+//3.this block return the links of netflix main page
+async function returnlink(){ 
+try{
     const html = await getdata('https://www.netflix.com/in/');
-    const links = await parsedata(html);
-        console.log(links)
+    const links = await crawldata(html);
+     return links;
+}
+catch(err){
+    console.log("error = ",err.message)
+} // 
 
 }
 
+//4.this block takes the links of the main netflix page converts them into html and take outs their links 
+async function crawlagain(newlink){
+    const links = await newlink();
+    const length = links.length;
 
+    const queue =[];
+    for(let i=0;i<length;i++){
+    try{
+    const nxtlink = await getdata(links[i]);
+
+    const $ = cheerio.load(nxtlink);
+
+$('a').each((index,element) =>{
+    const url = $(element).attr('href')
+     if(url){
+         //const absoluteurl = new URL(url,'https://www.netflix.com/in/');
+
+         const regex = /^http:\/\/|^https:\/\//i.test(url);
+
+        if(regex)
+         queue.push(url);
+     }
+
+})}
+catch(error){
+    console.log('error ocoured =',error)
+}
+    }
+    return queue;
+}
+
+//5.this block just prints the links
+async function main(){
+    const links = await returnlink();
+    console.log(links);
+const queue = await crawlagain(returnlink);
+
+console.log(queue);
+console.log('hello')
+}
 
 main();
