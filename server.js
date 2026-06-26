75c2b6875da908b4750c156714190847faa24496
Amay <amayrana86@gmail.com>
2026-06-26 08:23:06 +0530

    i removed unnecessary words lik as or and, and did stemming

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const fs = require('fs/promises');
 const { url } = require('inspector');
-
+const natural = require('natural');
 
 //defining the queue array and visited set array
  const queue =[];
@@ -36,7 +36,78 @@ $('a').each((index,element) =>{
 
 })
 
+//this function return the title and text of the page
+async function title_text(html)
+{
+const $ = await cheerio.load(html);
+
+//getting text from title
+const title = $('title').text().trim()
+
+//getting text from body
+const text = $('body').text().trim();
+const result =  new Set([text.toLowerCase(). replace(/[^\w\s]|_/g, "").trim().split(/\s+/)]);//tokenization
+
+return
+{ title,
+  result
+};
 }
+}
+
+//removing unecessary words like and ,or, the,is,or,as,an,a,for,by,
+const stopwords = new Set([
+  "a", "about", "above", "after", "again", "against", "all", "am",
+  "an", "and", "any", "are", "aren't", "as", "at",
+  "be", "because", "been", "before", "being", "below", "between",
+  "both", "but", "by",
+  "can", "can't", "cannot", "could", "couldn't",
+  "did", "didn't", "do", "does", "doesn't", "doing", "don't",
+  "down", "during",
+  "each",
+  "few", "for", "from", "further",
+  "had", "hadn't", "has", "hasn't", "have", "haven't", "having",
+  "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers",
+  "herself", "him", "himself", "his", "how", "how's",
+  "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is",
+  "isn't", "it", "it's", "its", "itself",
+  "let's",
+  "me", "more", "most", "mustn't", "my", "myself",
+  "no", "nor", "not",
+  "of", "off", "on", "once", "only", "or", "other", "ought",
+  "our", "ours", "ourselves", "out", "over", "own",
+  "same", "she", "she'd", "she'll", "she's", "should",
+  "shouldn't", "so", "some", "such",
+  "than", "that", "that's", "the", "their", "theirs", "them",
+  "themselves", "then", "there", "there's", "these", "they",
+  "they'd", "they'll", "they're", "they've", "this", "those",
+  "through", "to", "too",
+  "under", "until", "up",
+  "very",
+  "was", "wasn't", "we", "we'd", "we'll", "we're", "we've",
+  "were", "weren't", "what", "what's", "when", "when's", "where",
+  "where's", "which", "while", "who", "who's", "whom", "why",
+  "why's", "with", "won't", "would", "wouldn't",
+  "you", "you'd", "you'll", "you're", "you've", "your", "yours",
+  "yourself", "yourselves"
+])
+for(const word of result)
+{
+    if(stopwords.has(word))
+    {
+        result.delete(word);
+
+    }
+}
+
+//Stemming
+const stemmer = natural.PorterStemmer;
+const stemmed = new Set();//new set containing the words of text
+for(const word of result){
+    stemmed.add(stemmer.stem(word));
+
+}
+
 //3.this block return the links of netflix main page
 async function returnlink()
 { 
