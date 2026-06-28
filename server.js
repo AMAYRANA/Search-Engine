6853cba036142ed523ea2e72001861447ff3ad14
Amay <amayrana86@gmail.com>
2026-06-28 22:31:51 +0530

    built inverted index,it prints in what documents does a certain word appear

--- server.js
+++ server.js
@@ -35,27 +35,41 @@ $('a').each((index,element) =>{
      }
 
 })
+}
 
 //this function return the title and text of the page
-async function title_text(html)
+ function title_text(html)
 {
-const $ = await cheerio.load(html);
+const $ =  cheerio.load(html);
 
 //getting text from title
+$([
+    'script',
+    'style',
+    'noscript',
+    'svg',
+    'canvas',
+    'iframe',
+    'header',
+    'footer',
+    'nav',
+    'aside',
+    'form'
+].join(',')).remove();  //removing unwanted selectors in css and js
 const title = $('title').text().trim()
 
 //getting text from body
 const text = $('body').text().trim();
-const result =  new Set([text.toLowerCase(). replace(/[^\w\s]|_/g, "").trim().split(/\s+/)]);//tokenization
+const result =  new Set(text.toLowerCase(). replace(/[^\w\s]|_/g, "").trim().split(/\s+/));//tokenization
 
-return
-{ title,
+return{ 
+    title,
   result
 };
-}
-}
+};
 
 //removing unecessary words like and ,or, the,is,or,as,an,a,for,by,
+function remove_stp(result){
 const stopwords = new Set([
   "a", "about", "above", "after", "again", "against", "all", "am",
   "an", "and", "any", "are", "aren't", "as", "at",
@@ -99,13 +113,18 @@ for(const word of result)
 
     }
 }
+return result;
+}
 
 //Stemming
+function stem(result){
 const stemmer = natural.PorterStemmer;
 const stemmed = new Set();//new set containing the words of text
 for(const word of result){
     stemmed.add(stemmer.stem(word));
 
+}
+return stemmed;
 }
 
 //3.this block return the links of netflix main page
@@ -128,13 +147,20 @@ async function crawlagain(link){
    // const links = await newlink();
     //const length = link.length;
 
-    visited.add(link);//pushed the link which is used ,into visited array
+    
 
     //for(let i=0;i<length;i++){
     try{
-    const nxtlink = await getdata(link);//strips the html from link
+    const nxthtml = await getdata(link);//strips the html from link
 
-    const $ = cheerio.load(nxtlink);
+    visited.add(link);//pushed the link which is used ,into visited array
+         const text = title_text(nxthtml)//this has title and text
+         let result = text.result;//set of words returned and stored into result
+          result = remove_stp(result);//remove stopwords
+          result = stem(result);//stem them
+
+
+    const $ = cheerio.load(nxthtml);            
 
 $('a').each((index,element) =>{
     const url = $(element).attr('href')
@@ -142,32 +168,59 @@ $('a').each((index,element) =>{
          //const absoluteurl = new URL(url,'https://www.netflix.com/in/');
          const regex = /^http:\/\/|^https:\/\//i.test(url);
 
-        if(regex &&  !queue.includes(url) )
+        if(regex &&  !queue.includes(url) && queue.length<50)
          queue.push(url);
      }
 
-})}
+}); 
+ return result;
+}
 catch(error){
     console.log('error ocoured =',error.message);
     console.log('error ocoured =',error.code);
+    return new Set();
 }
 }
 
+const documents = [];
 async function main(){
 
-    await returnlink();// the the links of the first page of netflix is added to queue
+    await returnlink();// the links of the first page of netflix is added to queue
 
 
-for(let i = 0;i<queue.length;i++){
+for(let i = 0; i<queue.length && i<
+    50;i++){
     if(!visited.has(queue[i])){
         console.log("Queue size:", queue.length);
-        await crawlagain(queue[i])
-    }
-}
+      let result =  await crawlagain(queue[i])//takes the first link from queue takes its links out and add them into queue and in visitid(set) also and repeat for next link
+      for(const word of result){
+      documents.push({
+       text:word,
+        id: i
+     });};
+      
+
+}}
 console.log(queue);
 }
  
-main();
+
+async function start() {
+    await main();
+    const index = new Map();
+for(const doc of documents){
+    if(!index.has(doc.text)){
+       index.set(doc.text,[]);
+    }
+    index.get(doc.text).push(doc.id);
+}
+console.log(index);
+
+
+}
+
+start();
+
 
 
 //things to do later 
